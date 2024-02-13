7a77be0df95bbff3c4eeec5bce04431a833d443a
Corentin Lejeune <Corentin.lejeune@orange.fr>
2024-02-13 16:04:42 +0100

    Ajout Redux

---
+++ src/Components/store.js
@@ -0,0 +1,41 @@
+import { createStore } from 'redux';
+
+const initialState = {
+    isLoggedIn: false,
+    user: null,
+  };
+
+  const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'LOGIN':
+        return {
+          ...state,
+          isLoggedIn: true,
+          user: action.payload.user
+        };
+      case 'LOGOUT':
+        return {
+          ...state,
+          isLoggedIn: false,
+          user: null
+        };
+      default:
+        return state;
+    }
+  };
+
+  export const login = (user) => ({
+    type: 'LOGIN',
+    payload: {
+      user
+    }
+  });
+  
+  export const logout = () => ({
+    type: 'LOGOUT'
+  });
+  
+
+const store = createStore(rootReducer);
+
+export default store;
